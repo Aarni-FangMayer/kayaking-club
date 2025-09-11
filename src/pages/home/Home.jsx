54c42a0d0026ea1027d0073b56a18ab733179df1
Aarni <aarni.fangmayer@gmail.com>
2025-09-11 17:48:05 +0300

    refactor: extract MainSectionHome from Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,22 +1,14 @@
-import React from 'react'
-import StaticLayout from '../../components/layouts/staticLayout/StaticLayout'
-import './home.css'
+import React from "react";
+import StaticLayout from "../../components/layouts/staticLayout/StaticLayout";
+import MainSectionHome from "./page_sections/MainSectionHome";
+import "./home.css";
 
 const Home = () => {
   return (
     <StaticLayout>
-      <div className="contentWrapper">
-        <div className="contentWrapper__image">
-
-        </div>
-        <div className="contentWrapper__text">
-          <h1><span>kayaking </span>club</h1>
-          
-        </div>
-      </div>
+      <MainSectionHome />
     </StaticLayout>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
---
+++ src/pages/home/page_sections/MainSectionHome.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ArrowBlueButton from "../../../components/buttons/arrow_blue/ArrowBlueButton";
+import SliderSmall from "../../../components/sliders/slider_small/SliderSmall";
+import './mainSectionHome.css'
+
+const MainSectionHome = () => {
+  return (
+    <div className="home-main">
+      <div className="home-content">
+        <div className="home-content__image">
+          <SliderSmall />
+        </div>
+        <div className="home-content__text">
+          <h1 className="home-content__title">
+            <span>kayaking </span>club
+          </h1>
+          <p className="home-content__description">
+            Our tours are designed to fit your pace, your passion, <br />
+            and adventure.
+          </p>
+          <ArrowBlueButton text={"Join us here"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainSectionHome;
