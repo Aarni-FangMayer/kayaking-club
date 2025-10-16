f3909e0d5995781401e40acd10f246dd889608e5
Aarni <aarni.fangmayer@gmail.com>
2025-10-16 18:43:42 +0300

    refactor: clean up and improve structure of Account component

--- src/pages/account/Account.jsx
+++ src/pages/account/Account.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import CardList from "../../components/lists/CardList";
 import StaticLayoutSingle from "../../components/layouts/staticLayoutSingle/StaticLayoutSingle";
+import TextBlockWithHighlights from "../../components/shared/TextBlockWithHighlights";
+import ProfileEditor from "../../components/shared/ProfileEditor";
 import "./account.css";
+import AvatarImage from '../../assets/images/avatar.png'
 
 const Account = () => {
   const bookedTours = [
@@ -30,58 +33,17 @@ const Account = () => {
     <StaticLayoutSingle>
       <div className="account">
         <div className="account__user">
-          <div className="account__intro">
-            <h2 className="account__greeting">Hello, Name Name</h2>
-            <div className="account__intro-text">
-              <h3 className="account__subtitle">
-                Welcome to your personal account
-              </h3>
-              <p className="account__description">
-                Here you can change your personal information and see your
-                upcoming reservations. As a registered River Pulse member, you
-                can comment on blog posts and interact with other users in the
-                comments.{" "}
-                <span className="account__highlight">
-                  Visit the Routes page to book your next trip!
-                </span>
-              </p>
-            </div>
-          </div>
-          <div className="account__edit">
-            <h3 className="account__edit-title">Edit profile</h3>
-            <div className="account__edit-content">
-              <div className="account__avatar">
-                <img
-                  src=""
-                  alt="User avatar"
-                  className="account__avatar-image"
-                />
-                <button className="account__avatar-button">
-                  change avatar
-                </button>
-              </div>
-              <div className="account__form">
-                <form className="account__form-fields">
-                  <input
-                    type="text"
-                    placeholder="Name Name"
-                    className="account__input"
-                  />
-                  <input
-                    type="email"
-                    placeholder="email@gmail.com"
-                    className="account__input"
-                  />
-                  <input
-                    type="number"
-                    placeholder="40 856 6545"
-                    className="account__input"
-                  />
-                </form>
-                <button className="account__save-button">Save</button>
-              </div>
-            </div>
-          </div>
+          <TextBlockWithHighlights
+            title={"Hello, Name Name"}
+            subtitle={"Welcome to your personal account"}
+            describtion={
+              "Here you can change your personal information and see your upcoming reservations. As a registered River Pulse member, you can comment on blog posts and interact with other users in the comments."
+            }
+            highlight={"Visit the Routes page to book your next trip!"}
+            addRouteBtnText={""}
+            addPostBtnText={""}
+          />
+          <ProfileEditor avatarImg={AvatarImage} />
         </div>
         <div className="account__bookings">
           <CardList
