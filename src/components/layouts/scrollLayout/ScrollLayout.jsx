de915d071129c31896bbbdde8383588d9195c7e9
Aarni <aarni.fangmayer@gmail.com>
2025-11-01 21:19:46 +0200

    chore: minor code cleaning

--- src/components/layouts/scrollLayout/ScrollLayout.jsx
+++ src/components/layouts/scrollLayout/ScrollLayout.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "./scrollLayout.css";
 
 const ScrollLayout = ({ sectionOne, sectionTwo, sectionThree, sectionFour, modalOpen, activeSection, setActiveSection, }) => {
