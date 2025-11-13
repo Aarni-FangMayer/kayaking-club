982db6fd8dabb1793288a7ce2f4a536e7fc80c1d
Aarni <aarni.fangmayer@gmail.com>
2025-11-13 21:21:30 +0200

    feat: add pagination to blog list with dynamic Prev/Next buttons

--- frontend/src/components/lists/CardList.jsx
+++ frontend/src/components/lists/CardList.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ArticleCardPromo from "../cards/article_card_promo/ArticleCardPromo";
 import PaginationControl from "../buttons/pagination_control/PaginationControl";
 
 import "./cardList.css";
 
 const CardList = ({ header, arr, subtitle, callback }) => {
+  const itemsPerPage = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(arr.length / itemsPerPage);
+
+  const displayedCards = arr.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div className="cars-list">
       <div className="cars-list__content">
         <h3 className="cars-list-header">{header}</h3>
-        {arr.map((card) => {
+        {displayedCards.map((card) => {
           return (
             <li key={card.id} onClick={() => callback(card)}>
-
               <ArticleCardPromo
                 title={card.title}
                 data={card.data}
@@ -24,7 +36,11 @@ const CardList = ({ header, arr, subtitle, callback }) => {
         })}
       </div>
       <div className="cars-list__controls">
-        <PaginationControl />
+        <PaginationControl
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
       </div>
     </div>
   );
