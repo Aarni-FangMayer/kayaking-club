275d0e98600f6a4e0191044c70bd7bb08cd0e297
Aarni <aarni.fangmayer@gmail.com>
2025-11-14 00:48:44 +0200

    feat: add checkAdmin and authenticate middleware for role and JWT verification

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,5 +1,46 @@
 const path = require("path");
 const logger = require("./logger");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const checkAdmin = (request, response, next) => {
+  const user = request.user;
+
+  if (!user) {
+    return response.status(401).json({ error: "Unauthorized" });
+  }
+
+  if (user.role !== "admin") {
+    return response.status(403).json({ error: "Forbidden: Admins only" });
+  }
+
+  next();
+}
+
+const authenticate = async (req, res, next) => {
+  const authHeader = req.get("Authorization");
+  if (!authHeader || !authHeader.toLowerCase().startsWith("bearer ")) {
+    return res.status(401).json({ error: "Token missing or invalid" });
+  }
+
+  const token = authHeader.substring(7);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+    if (!user) return res.status(401).json({ error: "User not found" });
+
+    req.user = {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+    };
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
@@ -42,6 +83,8 @@ const frontendHandler = (distPath) => {
 };
 
 module.exports = {
+  checkAdmin,
+  authenticate,
   requestLogger,
   unknownEndpoint,
   errorHandler,
