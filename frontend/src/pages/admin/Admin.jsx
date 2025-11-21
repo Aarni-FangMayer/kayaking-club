1e3c1a76d4d1ffa10b44d90cf1e3e9b11b799f2b
Aarni <aarni.fangmayer@gmail.com>
2025-11-22 01:52:34 +0200

    feat: connect admin panel to AuthContext and display real-time list of booked tours

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -1,41 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StaticLayoutSingle from "../../components/layouts/staticLayoutSingle/StaticLayoutSingle";
 import CardList from "../../components/lists/CardList";
 import TextBlockWithHighlights from "../../components/shared/TextBlockWithHighlights";
 import ProfileEditor from "../../components/shared/ProfileEditor";
+import RoutesModal from "../../components/modals/routesModal/RoutesModal";
+import SelectedTour from "../../pages/tours/toursCatalog/SelectedTour"
+import toursService from "../..//services/tours";
+import { useAuth } from "../../contexts/AuthContext";
 import "./admin.css";
 import AvatarImage from "../../assets/images/avatar.png";
 
 const Admin = () => {
-  const allRoutes = [
-    {
-      id: 1,
-      title: "Tour adventure #1",
-      data: "12 jan",
-    },
-    {
-      id: 2,
-      title: "Tour adventure #2",
-      data: "1 okt",
-    },
-    {
-      id: 3,
-      title: "Tour adventure #3",
-      data: "24 sep",
-    },
-    {
-      id: 4,
-      title: "Tour adventure #4",
-      data: "3 mar",
-    },
-  ];
+  const { userInfo } = useAuth();
+
+  const [allTours, setAllTours] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedTour, setSelectedTour] = useState("");
+
+  const closeModal = () => {
+    setModalOpen(false);
+    setSelectedTour("");
+  };
+
+  useEffect(() => {
+    toursService.getAll().then((response) => {
+      setAllTours(response.data);
+    });
+  }, []);
+
+  const allBookedToursList = allTours.filter(
+    (tour) => tour.accountId.length > 0
+  );
 
   return (
     <StaticLayoutSingle>
       <div className="account">
         <div className="account__user">
           <TextBlockWithHighlights
-            title={"Hello, Name Name"}
+            title={`Hello, ${userInfo.username}`}
             subtitle={"Welcome to admin page"}
             describtion={
               "Here you can change your personal information, see all upcoming expeditions, write new posts to blog and add extra routes to booking. As a registered River Pulse member, you can also comment on blog posts and interact with other users in the comments."
@@ -49,11 +51,18 @@ const Admin = () => {
         <div className="account__bookings">
           <CardList
             header={"All Upcoming Expeditions"}
-            arr={allRoutes}
+            arr={allBookedToursList}
             subtitle={"expedition"}
+            callback={(card) => {
+              setSelectedTour(card);
+              setModalOpen(true);
+            }}
           />
         </div>
       </div>
+      <RoutesModal isModalOpen={modalOpen} closeModal={closeModal}>
+        <SelectedTour currentTour={selectedTour} />
+      </RoutesModal>
     </StaticLayoutSingle>
   );
 };
