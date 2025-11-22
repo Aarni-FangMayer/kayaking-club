e152884846e6132dabbb54bc9f2336d25f244225
Aarni <aarni.fangmayer@gmail.com>
2025-11-22 03:25:06 +0200

    refactor: removed duplicate functionality for opening a modal window from the component

--- frontend/src/components/cards/article_card/ArticleCard.jsx
+++ frontend/src/components/cards/article_card/ArticleCard.jsx
@@ -4,23 +4,16 @@ import BlogsModal from '../../modals/blogsModal/BlogsModal'
 import ArticleWithComments from '../../../pages/blogs/ArticleWithComments/ArticleWithComments'
 import './articleCard.css'
 
-const ArticleCard = ({ subtitle, description, image, textContent, article, btnText, setModalOpen }) => {
+const ArticleCard = ({ subtitle, description, article, btnText, setModalOpen, callback }) => {
   const [blogModalOpen, setBlogModalOpen] = useState(false);
 
-  const closeBlogModal = () => {
-    setBlogModalOpen(false);
-    setModalOpen(false);
-  }
   return (
     <div className="article-card">
         <div className="article-card__content">
             <h3 className="article-card__subtitle">{subtitle}</h3>
             <p className="article-card__description">{description}</p>
         </div>
-        <ArrowBlueButton clickEvent={() => {setBlogModalOpen(true); setModalOpen(true)}} text={btnText} />
-        <BlogsModal isModalOpen={blogModalOpen} closeModal={closeBlogModal}>
-          <ArticleWithComments title={subtitle} text={textContent} image={image} article={article} />
-        </BlogsModal>
+        <ArrowBlueButton clickEvent={() => {setBlogModalOpen(true); setModalOpen(true); callback && callback(article)}} text={btnText} />
     </div>
   )
 }
