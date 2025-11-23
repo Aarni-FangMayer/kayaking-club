1a6ea2be1d232f475ebbf345e5b551d178ce3ed6
Aarni <aarni.fangmayer@gmail.com>
2025-11-23 23:37:58 +0200

    feat: add avatar change button to ProfileForm and update styles

--- frontend/src/components/forms/ProfileForm.jsx
+++ frontend/src/components/forms/ProfileForm.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import ArrowBlueButton from "../buttons/arrow_blue/ArrowBlueButton";
 import "./profileForm.css";
 
-const ProfileForm = ({ placeholderName, placeholderEmail, placeholderPhone, btnText }) => {
+const ProfileForm = ({ placeholderName, placeholderEmail, placeholderPhone, avatarImg, btnText }) => {
   return (
     <div className="account__form">
+      <div className="account__avatar">
+        <img src={avatarImg} alt="" />
+        <button className="account__avatar-btn">change avatar</button>
+      </div>
       <form className="account__form-fields">
         <input type="text" placeholder={placeholderName} className="account__input" />
         <input
