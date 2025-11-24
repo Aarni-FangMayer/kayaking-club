b9ab4bc7e6bc0c25ca51aedcd4817035592aa4dd
Aarni <aarni.fangmayer@gmail.com>
2025-11-24 02:31:35 +0200

    feat: update BurgerMenu styles and add login & registration features with AuthContext support

--- frontend/src/components/forms/RegistrationForm.jsx
+++ frontend/src/components/forms/RegistrationForm.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import ArrowBlueButton from '../buttons/arrow_blue/ArrowBlueButton'
-import './registrationForm.css'
+import React, { useState } from "react";
+import ArrowBlueButton from "../buttons/arrow_blue/ArrowBlueButton";
+import "./registrationForm.css";
 
-const RegistrationForm = ({ title, subbutton, registerUser, className  }) => {
+const RegistrationForm = ({ title, subbutton, registerUser, className }) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -16,46 +16,59 @@ const RegistrationForm = ({ title, subbutton, registerUser, className  }) => {
       email,
       phone,
       password,
-      role: "user"
+      role: "user",
     });
+
+    setName("");
+    setEmail("");
+    setPhone("");
+    setPassword("");
   };
 
   return (
     <div className={`registration__form-block ${className || ""}`}>
-        <h3 className="registration__form-title" onSubmit={registerUser}>{title}</h3>
-        <form onSubmit={handleSubmit} className="registration__form">
-          <input
-            type="text"
-            placeholder="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="registration__form-field"
-          />
-          <input
-            type="email"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="registration__form-field"
-          />
-          <input
-            type="tel"
-            placeholder="phone number"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            className="registration__form-field"
-          />
-          <input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="registration__form-field"
-          />
-          {subbutton ? <button type='submit' className="subbutton">{subbutton}</button> : <ArrowBlueButton text={"Join us"} />}
-        </form>
-      </div>
-  )
-}
+      <h3 className="registration__form-title" onSubmit={registerUser}>
+        {title}
+      </h3>
+      <form onSubmit={handleSubmit} className="registration__form">
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="registration__form-field"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="registration__form-field"
+        />
+        <input
+          type="tel"
+          placeholder="phone number"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          className="registration__form-field"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="registration__form-field"
+        />
+        {subbutton ? (
+          <button type="submit" className="subbutton">
+            {subbutton}
+          </button>
+        ) : (
+          <ArrowBlueButton text={"Join us"} />
+        )}
+      </form>
+    </div>
+  );
+};
 
-export default RegistrationForm
+export default RegistrationForm;
--- frontend/src/components/navigation/burgerMenu/BurgerMenu.jsx
+++ frontend/src/components/navigation/burgerMenu/BurgerMenu.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../../../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
+import loginService from "../../../services/login";
+import userService from "../../../services/userService";
+import LoginForm from "../../forms/LoginForm";
+import RegistrationForm from "../../forms/RegistrationForm";
 import "./burgerMenu.css";
 
 const BurgerMenu = ({ menuOpen, setMenuOpen }) => {
   const { isAuth, userInfo, logout } = useAuth();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [registrationFormOpen, setRegistrationFormOpen] = useState(false);
+
+  const { setUserAuthState, setUserToken, setUserInformation } = useAuth();
 
   const navigate = useNavigate();
 
@@ -23,63 +33,136 @@ const BurgerMenu = ({ menuOpen, setMenuOpen }) => {
     setMenuOpen(false);
   };
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await loginService.login({ username, password });
+
+      if (response.status != 200) {
+        console.log("User is not authenticated", response.statusText);
+        setUserAuthState(false);
+        return;
+      }
+
+      setUserAuthState(true);
+      setUserToken(response.data.token);
+      setUserInformation(response.data);
+
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log("Error: wrong credentials", error);
+    }
+  };
+
   const handleLogoutClick = () => {
     logout();
     setMenuOpen(false);
   };
 
+  const registerUser = async (newUserData) => {
+    try {
+      await userService.register(newUserData);
+      console.log("User has been added to database");
+    } catch (error) {
+      console.log("Registration error", error);
+    }
+  };
+
   return (
-    <div className={`burgerMenu ${menuOpen ? "burgerMenu--open" : ""}`}>
-      <button className="burgerMenu__close" onClick={() => setMenuOpen(false)}>
-        ✕
-      </button>
-      <ul className="burgerMenu__list">
-        <li>
-          <button className="burgerMenu__link" onClick={handleAccountClick}>
-            Account
-          </button>
-        </li>
-        <li>
-          <button
-            className="burgerMenu__link"
-            onClick={() => {
-              navigate("/tours");
-              setMenuOpen(false);
-            }}
-          >
-            Routes & Prices
-          </button>
-        </li>
-        <li>
-          <button
-            className="burgerMenu__link"
-            onClick={() => {
-              navigate("/blogs");
-              setMenuOpen(false);
-            }}
-          >
-            Our Blog
-          </button>
-        </li>
-        <li>
-          {isAuth ? (
+    <>
+      <div className={`burgerMenu ${menuOpen ? "burgerMenu--open" : ""}`}>
+        <button
+          className="burgerMenu__close"
+          onClick={() => setMenuOpen(false)}
+        >
+          ✕
+        </button>
+        <ul className="burgerMenu__list">
+          <li>
             <button
-              onClick={handleLogoutClick}
-              className="loginBtn"
+              className="burgerMenu__link-btn"
+              onClick={handleAccountClick}
             >
-              Logout
+              Account
             </button>
-          ) : (
+          </li>
+          <li>
             <button
-              onClick={() => console.log("login btn has clicked")}
-              className="loginBtn"
+              className="burgerMenu__link-btn"
+              onClick={() => {
+                navigate("/");
+                setMenuOpen(false);
+              }}
             >
-              Login or register
+              About us
             </button>
-          )}
-        </li>
-      </ul>
-    </div>
+          </li>
+          <li>
+            <button
+              className="burgerMenu__link-btn"
+              onClick={() => {
+                navigate("/tours");
+                setMenuOpen(false);
+              }}
+            >
+              Routes & Prices
+            </button>
+          </li>
+          <li>
+            <button
+              className="burgerMenu__link-btn"
+              onClick={() => {
+                navigate("/blogs");
+                setMenuOpen(false);
+              }}
+            >
+              Our Blog
+            </button>
+          </li>
+          <li>
+            {isAuth ? (
+              <button onClick={handleLogoutClick} className="loginBtn">
+                Logout
+              </button>
+            ) : (
+              <div className="authentication">
+                <div className="authentication__login-block">
+                  Login to account
+                  <LoginForm
+                    handleLogin={handleLogin}
+                    username={username}
+                    setUsername={setUsername}
+                    password={password}
+                    setPassword={setPassword}
+                    subbutton={"Login"}
+                  />
+                </div>
+                <div className="authentication__registration-block">
+                  <p className="registration-text">
+                    Don't have a profile yet?{" "}
+                    <a
+                      className="registration-link"
+                      onClick={() =>
+                        setRegistrationFormOpen(!registrationFormOpen)
+                      }
+                    >
+                      Sign up now.
+                    </a>
+                  </p>
+                </div>
+                {registrationFormOpen && (
+                  <div className="authentication__login-block">
+                    Register here
+                    <RegistrationForm registerUser={registerUser} subbutton={"Join us"}/>
+                  </div>
+                )}
+              </div>
+            )}
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
