33657b2a53ba272ca8f1e8766055e796a190287d
Aarni <aarni.fangmayer@gmail.com>
2025-11-27 02:22:11 +0200

    feat: render RoutesModal via createPortal to overlay full page content

--- frontend/src/components/modals/routesModal/RoutesModal.jsx
+++ frontend/src/components/modals/routesModal/RoutesModal.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import { createPortal } from "react-dom";
 import "./routesModal.css";
 import CloseButton from '../../../assets/icons/close_button.png'
 
 const RoutesModal = ({ isModalOpen, closeModal, children }) => {
   if (!isModalOpen) return null;
-  return (
+
+  return createPortal(
     <div className="routes-modal__overlay" onClick={closeModal}>
-      <div
-        className="routes-modal__content"
-        onClick={(e) => e.stopPropagation()}
-      >
+      <div className="routes-modal__content" onClick={(e) => e.stopPropagation()}>
         <button className="routes-modal__close" onClick={closeModal}>
           <img src={CloseButton} alt="close button" />
         </button>
-        {/* {childWithProps} */}
         {children}
       </div>
-    </div>
+    </div>,
+    document.body
   );
 };
 
