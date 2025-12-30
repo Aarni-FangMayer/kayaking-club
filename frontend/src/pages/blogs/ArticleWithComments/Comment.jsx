d34120cfc8013d3a8c5cd74b1dbb413ecac4d58c
Aarni <aarni.fangmayer@gmail.com>
2025-12-30 22:34:01 +0200

    feat: used react-nice-avatar library to get random avatar for users in comment

--- frontend/src/pages/blogs/ArticleWithComments/Comment.jsx
+++ frontend/src/pages/blogs/ArticleWithComments/Comment.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactionButton from "../../../components/buttons/reaction_button/ReactionButton";
 import "./comment.css";
 import AvatarImage from "../../../assets/images/avatar.png";
+import Avatar, { genConfig } from 'react-nice-avatar'
 
 const Comment = ({ comment }) => {
+  const config = genConfig() 
   return (
     <div key={comment.user_id} className="comment">
       <div className="comment__base-info">
-        <img className="comment__avatar" src={comment.user_avatar ? comment.user_avatar : AvatarImage} alt="" />
+        <Avatar style={{ width: '3rem', height: '3rem' }} {...config} />
         <div className="comment__user-info">
           <h4 className="comment__user-name">{comment.username}</h4>
           <div className="subtitle">{comment.comment_date}</div>
